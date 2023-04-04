2de5a4f08fdfedf8d084df985c7182b928805b35
Igor Chestnov <inchestnov@gmail.com>
2023-04-04 20:15:40 +0300

    Fourth

---
+++ src/main/java/ru/gb/lesson4/Tree.java
@@ -0,0 +1,157 @@
+package ru.gb.lesson4;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public class Tree {
+
+    private class Node {
+        private int value;
+        private Node left;
+        private Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node root;
+
+    public void add(int value) {
+        root = appendNode(root, value);
+    }
+
+    private Node appendNode(Node current, int value) {
+        if (current == null) {
+            return new Node(value);
+        }
+
+        if (current.value > value) {
+            current.left = appendNode(current.left, value);
+        } else if (current.value < value) {
+            current.right = appendNode(current.right, value);
+        }
+        return current;
+    }
+
+    public boolean contains(int value) {
+        return findNode(root, value) != null;
+    }
+
+    private Node findNode(Node current, int value) {
+        if (current == null) {
+            return null;
+        }
+
+        if (current.value > value) {
+            return findNode(current.left, value);
+        } else if (current.value < value) {
+            return findNode(current.right, value);
+        }
+        return current;
+    }
+
+    public void remove(int value) {
+        root = removeNode(root, value);
+    }
+
+    private Node removeNode(Node current, int value) {
+        if (current == null) {
+            return null;
+        }
+
+        if (current.value > value) {
+            current.left = removeNode(current.left, value);
+            return current;
+        } else if (current.value < value) {
+            current.right = removeNode(current.right, value);
+            return current;
+        }
+
+        // Нужно удалить текущий узел.
+        // 3 случая:
+        // 1. Нет дочерних узлов
+        if (current.left == null && current.right == null) {
+            return null;
+        }
+
+        // 2. Есть только 1 дочерний узел
+        if (current.left == null) { //  && current.right != null
+            return current.right;
+        }
+        if (current.right == null) { // && current.left != null
+            return current.left;
+        }
+
+        // 3. Есть оба дочерних узла
+        // Нужно найти минимальный элемент в правом поддереве
+        Node smallestNodeOnTheRight = findFirst(current.right);
+        int smallestValueOnTheRight = smallestNodeOnTheRight.value;
+        // Вставить его значение в текущий узел
+        current.value = smallestValueOnTheRight;
+        // И удалить этот найденный минимальный узел у правого поддерева
+        current.right = removeNode(current.right, smallestValueOnTheRight);
+        return current;
+    }
+
+    public int findFirst() {
+        if (root == null) {
+            throw new NoSuchElementException();
+        }
+
+        return findFirst(root).value;
+    }
+
+    private Node findFirst(Node current) {
+        if (current.left != null) {
+            return findFirst(current.left);
+        }
+        return current;
+    }
+
+    public void dfs(Consumer<Integer> valueConsumer) {
+        dfsInternal(root, valueConsumer);
+    }
+
+    private void dfsInternal(Node current, Consumer<Integer> valueConsumer) {
+        if (current != null) {
+            dfsInternal(current.left, valueConsumer);
+            valueConsumer.accept(current.value);
+            dfsInternal(current.right, valueConsumer);
+        }
+    }
+
+    public void bfs(Consumer<Integer> valueConsumer) {
+        bfsInternal(valueConsumer);
+    }
+
+    private void bfsInternal(Consumer<Integer> valueConsumer) {
+        if (root == null) {
+            return;
+        }
+
+        // FIFO
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            valueConsumer.accept(node.value);
+
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+
+    public int size() {
+        final int[] counter = new int[1];
+        // или dfs
+        bfs(n -> counter[0]++);
+        return counter[0];
+    }
+
+}
---
+++ src/main/java/ru/gb/lesson4/TreeMain.java
@@ -0,0 +1,60 @@
+package ru.gb.lesson4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeMain {
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+
+        tree.add(7);
+        tree.add(3);
+        tree.add(9);
+        tree.add(2);
+        tree.add(1);
+        tree.add(5);
+        tree.add(4);
+        tree.add(6);
+        tree.add(8);
+
+        List<Integer> dfsOrderItems = new ArrayList<>();
+        tree.dfs(dfsOrderItems::add);
+//        tree.dfs(integer -> dfsOrderItems.add(integer));
+        System.out.println(dfsOrderItems);
+
+        List<Integer> bfsOrderItems = new ArrayList<>();
+        tree.bfs(bfsOrderItems::add);
+        System.out.println(bfsOrderItems);
+
+        System.out.println(tree.size());
+
+        System.out.println(tree.contains(5)); // true
+        System.out.println(tree.contains(2)); // true
+        System.out.println(tree.contains(10)); // true
+        System.out.println(tree.contains(-1)); // false
+        System.out.println(tree.contains(4)); // false
+
+        System.out.println(tree.findFirst()); // 2
+        tree.remove(2);
+        System.out.println(tree.findFirst()); // 5
+        tree.remove(5);
+        System.out.println(tree.findFirst()); // 10
+        tree.remove(10);
+
+        System.out.println(tree.findFirst()); // exception
+
+        // dfs depth-first-search  // поиск в глубину
+        // bfs breath-first-search // поиск в ширину
+        System.out.println(tree); // []
+
+    }
+
+
+    // HASH TABLE
+    // [null, null, null, null, null, null, null, null] size = 8
+    // element, hash(element) = 18 -- пример
+    // 18 % 8 -> 2 - остаток от деления на размер
+    // [null, null, [element], null, null, null, null, null] size = 8
+
+}
